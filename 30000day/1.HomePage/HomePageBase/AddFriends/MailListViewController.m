2253a6914e3d1d15216bdf946224bf48b4d38b6f
hongwei <760302648@qq.com>
2016-02-03 14:35:57 +0800

    完成搜索好友通讯录好友基本界面

--- 30000day/1.HomePage/HomePageBase/AddFriends/MailListViewController.m
+++ 30000day/1.HomePage/HomePageBase/AddFriends/MailListViewController.m
@@ -9,9 +9,16 @@
 #import "MailListViewController.h"
 #import <AddressBook/AddressBook.h>
 #import "mailList.h"
+#import "MailListTableViewCell.h"
 
 @interface MailListViewController ()
-@property (nonatomic,strong)NSMutableArray* mailListArray;
+@property (nonatomic,strong)NSMutableArray *mailListArray;
+@property (weak, nonatomic) IBOutlet UITableView *mailListTable;
+
+@property (weak, nonatomic) IBOutlet UITextField *searchText;
+@property (weak, nonatomic) IBOutlet UIView *searchSuperView;
+@property (weak, nonatomic) IBOutlet UIButton *cancelBtn;
+
 @end
 
 @implementation MailListViewController
@@ -19,20 +26,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    UIImageView *image = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"search.png"]];
+    self.searchText.leftView = image;
+    self.searchText.leftViewMode = UITextFieldViewModeUnlessEditing;
+    
+    [self.searchSuperView.layer setBorderWidth:1.0];
+    [self.searchSuperView.layer setBorderColor:VIEWBORDERLINECOLOR.CGColor];
+    
     self.mailListArray=[NSMutableArray array];
     [self loadPerson];
     
-    
-    for (int i=0; i<self.mailListArray.count; i++) {
-        mailList* ms=self.mailListArray[i];
-        NSLog(@"%@",ms.name);
-        NSLog(@"%@",ms.mobilePhone);
-    }
+    [self.mailListTable setDelegate:self];
+    [self.mailListTable setDataSource:self];
     
 }
 
-- (void)loadPerson
-{
+- (void)loadPerson{
     ABAddressBookRef addressBookRef = ABAddressBookCreateWithOptions(NULL, NULL);
     
     if (ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusNotDetermined) {
@@ -57,8 +66,7 @@
     }
 }
 
-- (void)copyAddressBook:(ABAddressBookRef)addressBook
-{
+- (void)copyAddressBook:(ABAddressBookRef)addressBook{
     CFIndex numberOfPeople = ABAddressBookGetPersonCount(addressBook);
     CFArrayRef people = ABAddressBookCopyArrayOfAllPeople(addressBook);
     
@@ -102,6 +110,54 @@
     }
 }
 
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.mailListArray.count;
+}
+
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    return nil;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return 0;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *ID=@"MailListCell";
+    MailListTableViewCell* cell=[tableView dequeueReusableCellWithIdentifier:ID];
+    if (cell==nil) {
+        cell=[[[NSBundle mainBundle] loadNibNamed:@"MailListTableViewCell" owner:nil options:nil]lastObject];
+    }
+    mailList* ml=self.mailListArray[indexPath.row];
+    cell.textLabel.text=ml.name;
+    
+    [self createBtn:cell];
+    return cell;
+}
+
+-(void)createBtn:(UITableViewCell *)cell{
+    UIButton *invitationBtn=[UIButton buttonWithType:UIButtonTypeCustom];
+    [invitationBtn setTitle:@"邀请" forState:UIControlStateNormal];
+    [invitationBtn.titleLabel setTextColor:[UIColor whiteColor]];
+    [invitationBtn setBackgroundColor:[UIColor colorWithRed:73.0/255 green:117.0/255 blue:188.0/255 alpha:1.0]];
+    invitationBtn.layer.cornerRadius=6;
+    invitationBtn.layer.masksToBounds=YES;
+    [invitationBtn addTarget:self action:@selector(invitationBtnClick) forControlEvents:UIControlEventTouchUpInside];
+    invitationBtn.translatesAutoresizingMaskIntoConstraints=NO;
+    [cell addSubview:invitationBtn];
+    
+    [cell addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[Healthybtn(61)]-15-|" options:0 metrics:nil views:@{@"Healthybtn":invitationBtn}]];
+    [cell addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[Healthybtn(30)]" options:0 metrics:nil views:@{@"Healthybtn":invitationBtn}]];
+    [cell addConstraint:[NSLayoutConstraint constraintWithItem:invitationBtn attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:cell attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0]];
+}
+
+-(void)invitationBtnClick{
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
