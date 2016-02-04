7445950a93067b5204a2abb7bb868769b5c7897f
GuoJia <546061679@qq.com>
2016-02-04 18:31:25 +0800

    修改bug

---
+++ 30000day/1.HomePage/HomePageBase/AddFriends/model/AddressBookModel.h
@@ -0,0 +1,13 @@
+//
+//  AddressBookModel.h
+//  30000day
+//
+//  Created by GuoJia on 16/2/4.
+//  Copyright © 2016年 GuoJia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AddressBookModel : NSObject
+
+@end
---
+++ 30000day/1.HomePage/HomePageBase/AddFriends/model/AddressBookModel.m
@@ -0,0 +1,13 @@
+//
+//  AddressBookModel.m
+//  30000day
+//
+//  Created by GuoJia on 16/2/4.
+//  Copyright © 2016年 GuoJia. All rights reserved.
+//
+
+#import "AddressBookModel.h"
+
+@implementation AddressBookModel
+
+@end
