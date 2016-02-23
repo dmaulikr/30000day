f11e7fe77b805787a7677ba240a0de89805a311a
hongwei <760302648@qq.com>
2016-02-23 11:21:35 +0800

    封装首页

---
+++ 30000day/1.HomePage/HomePageBase/NavigationBarView.h
@@ -0,0 +1,14 @@
+//
+//  NavigationBarView.h
+//  30000day
+//
+//  Created by wei on 16/2/23.
+//  Copyright © 2016年 GuoJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationBarView : UIView
+
+-(instancetype)initWithFrame:(CGRect)frame withWidth:(CGFloat)width withArray:(NSArray *)arr;
+@end
