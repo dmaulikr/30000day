b16c63922d7d169e75f3b3c4d7a3c45c61def46b
hongwei <760302648@qq.com>
2016-02-23 17:00:46 +0800

    资讯

---
+++ 30000day/2.Information/controller/view/InfomationTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  InfomationTableViewCell.m
+//  30000day
+//
+//  Created by wei on 16/2/23.
+//  Copyright © 2016年 GuoJia. All rights reserved.
+//
+
+#import "InfomationTableViewCell.h"
+
+@implementation InfomationTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
