b6eb988a30fb47de9a8b6a6b17fa8eceb860e903
hongwei <760302648@qq.com>
2016-02-23 15:28:11 +0800

    优化MainPageBaseViewController

--- 30000day/1.HomePage/HomePageBase/MainPageBaseViewController.m
+++ 30000day/1.HomePage/HomePageBase/MainPageBaseViewController.m
@@ -15,6 +15,9 @@
 #import "MainViewController.h"
 #import "SignInViewController.h"
 
+#define BUTTON_WIDTH 60
+#define BUTTON_HEIGHT 39
+
 @interface MainPageBaseViewController () <UIScrollViewDelegate>
 
 @property (nonatomic,strong) UIButton *mainPageButton;//主页
@@ -42,6 +45,7 @@
     
     [self configUI];
     
+    NSLog(@"%lf",_scrollView.frame.origin.y);
     //*******************进行用户登录判断************************************************/
     if (![Common isUserLogin]) {//过去没有登录
         
@@ -79,88 +83,43 @@
 #pragma mark ----初始化UI界面
 - (void)configUI {
     
+    self.automaticallyAdjustsScrollViewInsets=NO;
+    
     _scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH,SCREEN_HEIGHT)];
-    
     _scrollView.delegate = self;
-    
     _scrollView.showsHorizontalScrollIndicator = NO;
-    
     _scrollView.pagingEnabled = YES;
-    
     _scrollView.delegate = self;
-    
     _scrollView.bounces = NO;
     
     [self.view addSubview:_scrollView];
     
     //设置子控制器
     MainViewController *mainPageController = [[MainViewController alloc] init];
-    
-    PersonViewController   *personViewController = [[PersonViewController alloc] init];
-    
-    MessageViewController  *messageViewController = [[MessageViewController alloc] init];
-    
+    PersonViewController *personViewController = [[PersonViewController alloc] init];
+    MessageViewController *messageViewController = [[MessageViewController alloc] init];
     CalendarViewController *calendarViewController = [[CalendarViewController alloc] init];
     
     [self addChildViewController:mainPageController];
-    
     [self addChildViewController:personViewController];
-    
     [self addChildViewController:messageViewController];
-    
     [self addChildViewController:calendarViewController];
-    
-    [_scrollView setFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
    
-    UIView *mainView = mainPageController.view;
-    
-    mainView.translatesAutoresizingMaskIntoConstraints = NO;
-    
-    UIView *personView = personViewController.view;
-    
-    personView.translatesAutoresizingMaskIntoConstraints = NO;
 
-    UIView *messageView = messageViewController.view;
+    [mainPageController.view setFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+    [_scrollView addSubview:mainPageController.view];
     
-     messageView.translatesAutoresizingMaskIntoConstraints = NO;
+    [personViewController.view setFrame:CGRectMake(SCREEN_WIDTH, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+    [_scrollView addSubview:personViewController.view];
     
-    UIView *calendarView  = calendarViewController.view;
+    [messageViewController.view setFrame:CGRectMake(SCREEN_WIDTH * 2, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+    [_scrollView addSubview:messageViewController.view];
     
-    calendarView.translatesAutoresizingMaskIntoConstraints = NO;
+    [calendarViewController.view setFrame:CGRectMake(SCREEN_WIDTH * 3, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+    [_scrollView addSubview:calendarViewController.view];
     
-    [_scrollView addSubview:mainView];
+    [_scrollView setContentSize:CGSizeMake(SCREEN_WIDTH * 4, 0)];
     
-    [_scrollView addSubview:personView];
-    
-    [_scrollView addSubview:messageView];
-    
-    [_scrollView addSubview:calendarView];
-    
-    [_scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[view(w)]" options:0 metrics:@{@"w":@(SCREEN_WIDTH)} views:@{@"view":mainView}]];
-    
-    [_scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[view(h)]" options:0 metrics:@{@"h":@(SCREEN_HEIGHT)} views:@{@"view":mainView}]];
-    
-    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:mainView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1 constant:0]];
-    
-    [_scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-x-[view(w)]" options:0 metrics:@{@"x":@(1*SCREEN_WIDTH),@"w":@(SCREEN_WIDTH)} views:@{@"view":personView}]];
-    
-    [_scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[view(h)]" options:0 metrics:@{@"h":@(SCREEN_HEIGHT)} views:@{@"view":personView}]];
-    
-    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:personView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1 constant:0]];
-    
-   
-    [_scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-x-[view(w)]" options:0 metrics:@{@"x":@(2*SCREEN_WIDTH),@"w":@(SCREEN_WIDTH)} views:@{@"view":messageView}]];
-    
-    [_scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[view(h)]" options:0 metrics:@{@"h":@(SCREEN_HEIGHT)} views:@{@"view":messageView}]];
-    
-    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:messageView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1 constant:0]];
-    
-    
-    [_scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-x-[view(w)]-0-|" options:0 metrics:@{@"x":@(3*SCREEN_WIDTH),@"w":@(SCREEN_WIDTH)} views:@{@"view":calendarView}]];
-    
-    [_scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[view(h)]" options:0 metrics:@{@"h":@(SCREEN_HEIGHT)} views:@{@"view":calendarView}]];
-    
-    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:calendarView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1 constant:0]];
     
     //创建顶部按钮
     [self createButton];
@@ -168,77 +127,57 @@
 
 - (void)createButton {
     
-    self.buttonParentView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width , 44)];
-    
+    self.buttonParentView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH , 44)];
     self.navigationItem.titleView = self.buttonParentView;
     
     _mainPageButton = [self buttonWithTitle:@"主页" numberAndTag:0];
-    
+    [_mainPageButton setTitleColor:BLUECOLOR forState:UIControlStateNormal];
+    [_mainPageButton setFrame:CGRectMake(0, 5, BUTTON_WIDTH, BUTTON_HEIGHT)];
     [self.buttonParentView addSubview:_mainPageButton];
     
-    _mainPageButton.translatesAutoresizingMaskIntoConstraints = NO;
-    
-    [_mainPageButton addConstraint:[NSLayoutConstraint constraintWithItem:_mainPageButton attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeWidth multiplier:1.0 constant:60]];
-    
-    [self.buttonParentView addConstraint:[NSLayoutConstraint constraintWithItem:_mainPageButton attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.buttonParentView attribute:NSLayoutAttributeLeft multiplier:1.0 constant:0]];
-    
-    [self.buttonParentView addConstraint:[NSLayoutConstraint constraintWithItem:_mainPageButton attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.buttonParentView attribute:NSLayoutAttributeTop multiplier:1.0 constant:5]];
     
     _personButton = [self buttonWithTitle:@"自己人" numberAndTag:1];
-    
+    [_personButton setFrame:CGRectMake(BUTTON_WIDTH, 5, BUTTON_WIDTH, BUTTON_HEIGHT)];
     [self.buttonParentView addSubview:_personButton];
-    
-    _personButton.translatesAutoresizingMaskIntoConstraints = NO;
-    
-    [_personButton addConstraint:[NSLayoutConstraint constraintWithItem:_personButton attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeWidth multiplier:1.0 constant:60]];
-    
-    [self.buttonParentView addConstraint:[NSLayoutConstraint constraintWithItem:_personButton attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:_mainPageButton attribute:NSLayoutAttributeRight multiplier:1.0 constant:0]];
-    
-    [self.buttonParentView addConstraint:[NSLayoutConstraint constraintWithItem:_personButton attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.buttonParentView attribute:NSLayoutAttributeTop multiplier:1.0 constant:5]];
+
     
      _newsButton = [self buttonWithTitle:@"消息" numberAndTag:2];
-    
+    [_newsButton setFrame:CGRectMake(BUTTON_WIDTH*2, 5, BUTTON_WIDTH, BUTTON_HEIGHT)];
     [self.buttonParentView addSubview:_newsButton];
     
-    _newsButton.translatesAutoresizingMaskIntoConstraints=NO;
-    
-    [_newsButton addConstraint:[NSLayoutConstraint constraintWithItem:_newsButton attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeWidth multiplier:1.0 constant:60]];
-    
-    [self.buttonParentView addConstraint:[NSLayoutConstraint constraintWithItem:_newsButton attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:_personButton attribute:NSLayoutAttributeRight multiplier:1.0 constant:0]];
-    
-    [self.buttonParentView addConstraint:[NSLayoutConstraint constraintWithItem:_newsButton attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.buttonParentView attribute:NSLayoutAttributeTop multiplier:1.0 constant:5]];
     
     _moreAgeButton = [self buttonWithTitle:@"天龄日历" numberAndTag:3];
-    
+    [_moreAgeButton setFrame:CGRectMake(BUTTON_WIDTH*3, 5, BUTTON_WIDTH, BUTTON_HEIGHT)];
     [self.buttonParentView addSubview:_moreAgeButton];
     
-    _moreAgeButton.translatesAutoresizingMaskIntoConstraints=NO;
-    
-    [_moreAgeButton addConstraint:[NSLayoutConstraint constraintWithItem:_moreAgeButton attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeWidth multiplier:1.0 constant:60]];
-    
-    [self.buttonParentView addConstraint:[NSLayoutConstraint constraintWithItem:_moreAgeButton attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:_newsButton attribute:NSLayoutAttributeRight multiplier:1.0 constant:0]];
-    
-    [self.buttonParentView addConstraint:[NSLayoutConstraint constraintWithItem:_moreAgeButton attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.buttonParentView attribute:NSLayoutAttributeTop multiplier:1.0 constant:5]];
+
     
     _bottomScrollView = [[UIView alloc]initWithFrame:CGRectMake(0, 42, 60, 2)];
-    
     [_bottomScrollView setBackgroundColor:BLUECOLOR];
-    
     [self.buttonParentView addSubview:_bottomScrollView];
     
+    
     self.addFriendsButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    
     [self.addFriendsButton setImage:[UIImage imageNamed:@"addFriends.png"] forState:UIControlStateNormal];
-    
     [self.addFriendsButton addTarget:self action:@selector(addFriendsClick) forControlEvents:UIControlEventTouchDown];
-    
     [self.buttonParentView addSubview:self.addFriendsButton];
-    
     self.addFriendsButton.translatesAutoresizingMaskIntoConstraints=NO;
     
-    [self.buttonParentView addConstraint:[NSLayoutConstraint constraintWithItem:self.addFriendsButton attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.buttonParentView attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0]];
+    [self.buttonParentView addConstraint:[NSLayoutConstraint constraintWithItem:self.addFriendsButton
+                                                                      attribute:NSLayoutAttributeCenterY
+                                                                      relatedBy:NSLayoutRelationEqual
+                                                                         toItem:self.buttonParentView
+                                                                      attribute:NSLayoutAttributeCenterY
+                                                                     multiplier:1.0
+                                                                       constant:0]];
     
-    [self.buttonParentView addConstraint:[NSLayoutConstraint constraintWithItem:self.addFriendsButton attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.buttonParentView attribute:NSLayoutAttributeRight multiplier:1.0 constant:0]];
+    [self.buttonParentView addConstraint:[NSLayoutConstraint constraintWithItem:self.addFriendsButton
+                                                                      attribute:NSLayoutAttributeRight
+                                                                      relatedBy:NSLayoutRelationEqual
+                                                                         toItem:self.buttonParentView
+                                                                      attribute:NSLayoutAttributeRight
+                                                                     multiplier:1.0
+                                                                       constant:0]];
 
 }
 
@@ -263,9 +202,7 @@
 #pragma mark 按钮监听方法
 - (void)tapButton:(UIButton *)button {
     
-    CGFloat imgW = [UIScreen mainScreen].bounds.size.width;
-    
-    [_scrollView setContentOffset:CGPointMake(button.tag*imgW,0) animated:YES];
+    [_scrollView setContentOffset:CGPointMake(button.tag*SCREEN_WIDTH,0) animated:YES];
     
     [UIView animateWithDuration:0.3 animations:^{
         
@@ -273,60 +210,7 @@
         
     }];
     
-    switch (button.tag) {
-            
-        case 0:
-            
-            [_mainPageButton setTitleColor:BLUECOLOR forState:UIControlStateNormal];
-            
-            [_moreAgeButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
-            [_personButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
-            [_newsButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
-            break;
-            
-        case 1:
-            
-            [_personButton setTitleColor:BLUECOLOR forState:UIControlStateNormal];
-            
-            [_newsButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
-            [_mainPageButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
-            [_moreAgeButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
-            break;
-            
-        case 2:
-            
-            [_newsButton setTitleColor:BLUECOLOR forState:UIControlStateNormal];
-            
-            [_personButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
-            [_mainPageButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
-            [_moreAgeButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
-            break;
-            
-        case 3:
-            
-            [_moreAgeButton setTitleColor:BLUECOLOR forState:UIControlStateNormal];
-            
-            [_mainPageButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
-            [_personButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
-            [_newsButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
-            break;
-            
-        default:
-            
-            break;
-    }
+    [self buttonTitleChange:button.tag];
 }
 
 #pragma mark ---- UIScrollViewDelegate
@@ -334,63 +218,48 @@
     
     CGPoint offset = scrollView.contentOffset;
     
-    int curPageNo = offset.x / _scrollView.bounds.size.width;
+    NSInteger curPageNo = offset.x / _scrollView.bounds.size.width;
     
     [UIView animateWithDuration:0.3 animations:^{
         
         [_bottomScrollView setFrame:CGRectMake(curPageNo*60, 42, 60, 2)];
     }];
     
-    switch (curPageNo) {
+    [self buttonTitleChange:curPageNo];
+}
+
+-(void)buttonTitleChange:(NSInteger)page{
+    
+    switch (page) {
             
         case 0:
-            
             [_mainPageButton setTitleColor:BLUECOLOR forState:UIControlStateNormal];
-            
             [_moreAgeButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
             [_personButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
             [_newsButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
             
             break;
-            
         case 1:
-            
             [_personButton setTitleColor:BLUECOLOR forState:UIControlStateNormal];
-            
             [_newsButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
             [_mainPageButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
             [_moreAgeButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
             
             break;
-            
         case 2:
-            
             [_newsButton setTitleColor:BLUECOLOR forState:UIControlStateNormal];
-            
             [_personButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
             [_mainPageButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
             [_moreAgeButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
             
             break;
-            
         case 3:
-            
             [_moreAgeButton setTitleColor:BLUECOLOR forState:UIControlStateNormal];
-            
             [_mainPageButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
             [_personButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            
             [_newsButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
             
             break;
-            
         default:
             
             break;
