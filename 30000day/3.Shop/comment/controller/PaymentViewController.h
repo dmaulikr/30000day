608378d4e6010302f083c73104586492ede6ff28
hongwei <760302648@qq.com>
2016-03-12 18:11:37 +0800

    提交

---
+++ 30000day/3.Shop/comment/controller/PaymentViewController.h
@@ -0,0 +1,13 @@
+//
+//  PaymentViewController.h
+//  30000day
+//
+//  Created by wei on 16/3/12.
+//  Copyright © 2016年 GuoJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaymentViewController : STRefreshViewController
+
+@end
