2d5e7ec6da10b33fb92b1bbf867518bfb30490d9
GuoJia <546061679@qq.com>
2016-03-17 17:17:39 +0800

    获取商家数据

---
+++ 30000day/3.Shop/base/model/ShopModel.m
@@ -0,0 +1,22 @@
+//
+//  ShopModel.m
+//  30000day
+//
+//  Created by GuoJia on 16/3/17.
+//  Copyright © 2016年 GuoJia. All rights reserved.
+//
+
+#import "ShopModel.h"
+
+@implementation ShopModel
+
+- (void)setValue:(id)value forUndefinedKey:(NSString *)key {
+    
+    if ([key isEqualToString:@"id"]) {
+        
+        self.shopId = value;
+    }
+}
+
+
+@end
