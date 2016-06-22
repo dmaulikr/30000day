9419143da297c0e38400c222c010ee67e2489524
WeiGe <3303205219@qq.com>
2016-06-22 15:35:13 +0800

    提交

---
+++ 30000day/4.My/about/view/AboutTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  AboutTableViewCell.h
+//  30000day
+//
+//  Created by WeiGe on 16/6/22.
+//  Copyright © 2016年 GuoJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutTableViewCell : UITableViewCell
+
+@end
